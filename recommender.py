ce3e834cee80053b7798310b7d549de757c2048f
mrunalikhopade <mrunalijnec@gmail.com>
2025-07-02 23:46:58 +0530

    Lazy load data to reduce memory usage

--- recommender.py
+++ recommender.py
@@ -1,60 +1,69 @@
+# recommender.py
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import difflib
 
-# Load datasets
-movies_raw = pd.read_csv('tmdb_5000_movies.csv')
-credits = pd.read_csv('tmdb_5000_credits.csv')
+movies_df = None
+similarity = None
 
-# Merge on title
-movies_df = movies_raw.merge(credits, on='title')
+def load_data():
+    global movies_df, similarity
 
-# Clean dataset
-movies_df = movies_df[['id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew',
-                       'vote_average', 'release_date']]
+    if movies_df is not None and similarity is not None:
+        return  # Already loaded
 
-def get_director(crew):
-    try:
-        for person in eval(crew):
-            if person['job'] == 'Director':
-                return person['name']
-    except:
+    movies_raw = pd.read_csv('tmdb_5000_movies.csv')
+    credits = pd.read_csv('tmdb_5000_credits.csv')
+
+    movies_df_raw = movies_raw.merge(credits, on='title')
+    movies_df_clean = movies_df_raw[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew',
+                                     'vote_average', 'release_date']]
+
+    def get_director(crew):
+        try:
+            for person in eval(crew):
+                if person['job'] == 'Director':
+                    return person['name']
+        except:
+            return ''
         return ''
-    return ''
 
-def get_top_names(text):
-    try:
-        return [person['name'] for person in eval(text)[:3]]
-    except:
-        return []
+    def get_top_names(text):
+        try:
+            return [person['name'] for person in eval(text)[:3]]
+        except:
+            return []
 
-movies_df['director'] = movies_df['crew'].apply(get_director)
-movies_df['cast'] = movies_df['cast'].apply(get_top_names)
-movies_df['genres'] = movies_df['genres'].apply(get_top_names)
-movies_df['keywords'] = movies_df['keywords'].apply(get_top_names)
+    movies_df_clean['director'] = movies_df_clean['crew'].apply(get_director)
+    movies_df_clean['cast'] = movies_df_clean['cast'].apply(get_top_names)
+    movies_df_clean['genres'] = movies_df_clean['genres'].apply(get_top_names)
+    movies_df_clean['keywords'] = movies_df_clean['keywords'].apply(get_top_names)
 
-# Combine text for similarity
-movies_df['comb'] = (
-    movies_df['overview'].fillna('') + ' ' +
-    movies_df['director'].fillna('') + ' ' +
-    movies_df['cast'].apply(lambda x: ' '.join(x)) + ' ' +
-    movies_df['genres'].apply(lambda x: ' '.join(x)) + ' ' +
-    movies_df['keywords'].apply(lambda x: ' '.join(x))
-)
-movies_df['comb'] = movies_df['comb'].fillna('')
+    movies_df_clean['comb'] = (
+        movies_df_clean['overview'].fillna('') + ' ' +
+        movies_df_clean['director'].fillna('') + ' ' +
+        movies_df_clean['cast'].apply(lambda x: ' '.join(x)) + ' ' +
+        movies_df_clean['genres'].apply(lambda x: ' '.join(x)) + ' ' +
+        movies_df_clean['keywords'].apply(lambda x: ' '.join(x))
+    )
 
-# TF-IDF
-vectorizer = TfidfVectorizer(stop_words='english')
-vector_matrix = vectorizer.fit_transform(movies_df['comb'])
-similarity = cosine_similarity(vector_matrix)
+    vectorizer = TfidfVectorizer(stop_words='english')
+    vector_matrix = vectorizer.fit_transform(movies_df_clean['comb'].fillna(''))
 
-# Main function
+    similarity_matrix = cosine_similarity(vector_matrix)
+
+    movies_df = movies_df_clean
+    similarity = similarity_matrix
+
+
 def get_recommendations(movie_name):
+    load_data()  # lazy load on first request
+
     movie_name = movie_name.lower()
     all_titles = movies_df['title'].str.lower().tolist()
-
     close_matches = difflib.get_close_matches(movie_name, all_titles, n=1, cutoff=0.5)
+
     if not close_matches:
         return []
 
@@ -70,6 +79,7 @@ def get_recommendations(movie_name):
         movie = movies_df.iloc[i]
         recommendations.append({
             'title': movie['title'],
+            'poster': "https://via.placeholder.com/300x450?text=No+Image",
             'genres': ', '.join(movie['genres']),
             'rating': movie['vote_average'],
             'year': movie['release_date'].split('-')[0] if pd.notna(movie['release_date']) else 'N/A'
