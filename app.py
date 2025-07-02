4b915f1929b091434c4ecdef1daf67fb224925e6
mrunalikhopade <mrunalijnec@gmail.com>
2025-07-02 23:23:28 +0530

    Clean app structure for deployment

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    recommendations = []
+    recommendations = None
     if request.method == 'POST':
-        movie = request.form['movie']
+        movie = request.form.get('movie', '')
         recommendations = get_recommendations(movie)
     return render_template('index.html', recommendations=recommendations)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# We DO NOT include app.run() here; Render uses gunicorn.
